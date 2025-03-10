76ab2b7f8c3abf6396fcbe6047698a8d3f87d849
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-03-10 10:38:39 +0545

    Comparing string

---
+++ string/stringcheck.cs
@@ -0,0 +1,19 @@
+using System;
+public class Stringcheck{
+    public static void Main(){
+        Console.Write("Enter first string: ");
+        string a=Console.ReadLine();
+        Console.Write("Enter second string: ");
+        string b=Console.ReadLine();
+        int s=a.CompareTo(b);
+        if(s==0){
+            Console.WriteLine("The strings are equal.");
+        }
+        else if(s<0){
+            Console.WriteLine("The string 1 is smaller.");
+        }
+        else{
+            Console.Write("The string 2 is larger.");
+        }
+}
+}
