eb8d914b587e4f4f9ad11b758596b9286ae4462d
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-03-17 21:18:47 +0545

    Greater than

---
+++ controlstatement/if.cs
@@ -0,0 +1,10 @@
+using System;
+public class SelectionIf{
+    public static void Main(){
+        int x=8;
+        if(x>5){
+            Console.WriteLine("x is greater than 5");
+        }
+        Console.WriteLine("x is "+x);
+    }
+}
