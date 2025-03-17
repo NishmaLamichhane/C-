ef62f5fb50a9bc7992984463753d1a1187b91f36
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-03-17 21:23:12 +0545

    Number is equal or not

---
+++ controlstatement/ifelseif.cs
@@ -0,0 +1,24 @@
+using System;
+public class IfElseIf
+{
+    public static void Main()
+    {
+        int number = 500;
+        if (number == 10)
+        {
+            Console.WriteLine("Number is 10");
+        }
+        else if (number == 20)
+        {
+            Console.WriteLine("Number is 20");
+        }
+        else if (number == 30)
+        {
+            Console.WriteLine("Number is 30");
+        }
+        else
+        {
+            Console.WriteLine("Number is not 10, 20 or 30");
+        }
+    }
+} 
