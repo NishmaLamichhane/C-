5ef40f2858900779ebe6643797eb907c522c1954
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-03-17 21:16:48 +0545

    Vote Eligible Case

---
+++ controlstatement/ifelse.cs
@@ -0,0 +1,16 @@
+using System;
+public class Ifelse
+{
+    public static void Main()
+    {
+        int age = 18;
+        if (age >= 18)
+        {
+            Console.WriteLine("You are eligible to vote");
+        }
+        else
+        {
+            Console.WriteLine("You are not eligible to vote");
+        }
+    }
+}
