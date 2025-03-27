3fe34a832046a0777b8da5b619c9ad91b534569f
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-03-27 13:23:58 +0545

    Perform multiplication of given two matrices

---
+++ array/multiplicationof2matrix.cs
@@ -0,0 +1,60 @@
+using System;
+
+class MatrixMultiplication
+{
+    static void Main()
+    {
+        int[,] matrix1 = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        int[,] matrix2 = {
+            { 9, 8, 7 },
+            { 6, 5, 4 },
+            { 3, 2, 1 }
+        };
+
+        int[,] result = MultiplyMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Resultant Matrix:");
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        if (cols1 != rows2)
+        {
+            throw new InvalidOperationException("Matrix multiplication is not possible with the given matrices.");
+        }
+
+        int[,] result = new int[rows1, cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                result[i, j] = 0;
+                for (int k = 0; k < cols1; k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
