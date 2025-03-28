3352613f8bfb054b180fbf9af6d231206b44aafe
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-03-28 13:22:15 +0545

    Multiplication table updated

--- questions/multiplication.cs
+++ questions/multiplication.cs
@@ -1,13 +1,23 @@
 //Write a C# program to generate multiplication table from 1 to 10.
 using System;
-public class Multiplication{
-    public static void Main(string[] args){
-        int i, j;
-        for(i=1; i<=10; i++){
-            for(j=1; j<=10; j++){
-                Console.Write(i*j + "\t");
+
+public class MultiplicationTable
+{
+    public static void Main()
+    {
+        Console.WriteLine("==== Multiplication Table (1 to 10) ====\n");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"Multiplication Table of {i}:");
+            Console.WriteLine("---------------------------");
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} X {j} = {i * j}");
             }
-            Console.WriteLine();
+
+            Console.WriteLine(); 
         }
     }
 }
